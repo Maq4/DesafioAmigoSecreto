0fc06337e2d4fafe17a29fe3748e17eec3bc58d8
Maq4 <juanacorreoparaclases@gmail.com>
2025-09-05 11:08:26 -0400

    creando funcion y procedimientos

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 //Arreglos
-let amigos = []
+//let amigos = [];
+
+//Funciones 
+
+//function agregarAmigos (){}
+//function limpiarListaexistente(){}
+//function ValidarEntrada (){}
+//function MostrarListadoAmigos (){}
+//function SortearAmigoSecreto(){}
+//function validarAmigos(params) {}
+//function ordenaraleatorio(){}   
+
+
+//function reiniciarJuego() {}
