baf2ca41191e9e2b314c164c85ed1e9269f7cf74
Maq4 <juanacorreoparaclases@gmail.com>
2025-09-05 11:49:59 -0400

    function seleccionar amigos

--- app.js
+++ app.js
@@ -3,13 +3,34 @@
 
 //Funciones 
 
-//function agregarAmigos (){}
-//function limpiarListaexistente(){}
-//function ValidarEntrada (){}
-//function MostrarListadoAmigos (){}
-//function SortearAmigoSecreto(){}
+function seleccionarAmigoAleatorio(amigos) {
+  if (amigos.length === 0) {
+    alert("La lista de amigos está vacía.");
+    return null;
+  }
+
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  return amigos[indiceAleatorio];
+}
+
+// Ejemplo de uso:
+const amigos = []; // Cambia esto para probar
+const amigoSeleccionado = seleccionarAmigoAleatorio(amigos);
+
+if (amigoSeleccionado !== null) {
+  console.log("Amigo seleccionado:", amigoSeleccionado);
+}
+
+const amigoSeleccionado = seleccionarAmigoAleatorio(amigos);
+console.log("Amigo seleccionado:", amigoSeleccionado);
+
+
 //function validarAmigos(params) {}
 //function ordenaraleatorio(){}   
 
 
 //function reiniciarJuego() {}
+//function agregarAmigos (){}
+//function limpiarListaexistente(){}
+//function ValidarEntrada (){}
+//function MostrarListadoAmigos (){}
